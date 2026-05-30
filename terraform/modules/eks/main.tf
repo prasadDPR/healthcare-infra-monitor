ed9366e2d19225df95e276c34cd06e90ce21d402
prasad <dprasad28062001@gmail.com>
2026-05-30 15:18:35 +0100

    Add Promtail app for log collection

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -100,3 +100,48 @@ resource "aws_eks_node_group" "monitoring" {
     aws_iam_role_policy_attachment.node_ecr,
   ]
 }
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = aws_eks_cluster.healthcare_monitor.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.37.0-eksbuild.1"
+  resolve_conflicts_on_create = "OVERWRITE"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
+}
+
+resource "aws_iam_role" "ebs_csi" {
+  name = "healthcare-ebs-csi-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(aws_eks_cluster.healthcare_monitor.identity[0].oidc[0].issuer, "https://", "")}"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_eks_cluster.healthcare_monitor.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
+          "${replace(aws_eks_cluster.healthcare_monitor.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.healthcare_monitor.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.healthcare_monitor.identity[0].oidc[0].issuer
+}
