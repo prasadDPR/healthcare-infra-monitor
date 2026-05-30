690ba6af0a0eb7ce03f3b1c908b016ef6c332ea8
prasad <dprasad28062001@gmail.com>
2026-05-30 14:40:00 +0100

    Upgrade Loki to 3.x compatible with Grafana 11

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -89,9 +89,9 @@ resource "aws_eks_node_group" "monitoring" {
   instance_types  = ["t3.small"]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 4
-    min_size     = 2
+    desired_size = 3
+    max_size     = 5
+    min_size     = 3
   }
 
   depends_on = [
